aafe2019f1d24200a8e584572d4040e223e028a9
yurimeeee <kplhn6@gmail.com>
2023-10-24 11:27:22 +0900

    바인딩 문법

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,12 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it!"  msg2="Hello Vue.js" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/template">template 문법</RouterLink>
       </nav>
     </div>
   </header>
