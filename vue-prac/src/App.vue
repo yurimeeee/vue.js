29f409b21a0bed32202295c5361e96d6e91b206a
yurimeeee <kplhn6@gmail.com>
2023-12-12 00:10:59 +0900

    prac

---
+++ vue-prac/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <YurimHappy mss="happy Yurimmmmmm" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import YurimHappy from './components/Yurim.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    YurimHappy
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-prac/src/components/Yurim.vue
@@ -0,0 +1,50 @@
+<template>
+  <h2>{{ mss }}</h2>
+  <p v-if="seen">이제 나를 볼 수 있어요</p>
+  <ol>
+    <li v-for="todo in todos" :key="todo.text" :id="todo.text">
+      {{ todo.text }}
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  name: 'YurimHappy',
+  props: {
+    mss: String
+  },
+  data: function () {
+    return {
+      seen: true,
+      todos: [
+        { text: 'JavaScript 배우기' },
+        { text: 'Vue 배우기' },
+        { text: '무언가 멋진 것을 만들기' }
+      ]
+    };
+  },
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
